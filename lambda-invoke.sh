0f315f7ffaa555cad2802e39a4f11a96f884e94c
simrangulati <simrangulati327@gmail.com>
2025-07-01 19:47:25 +0530

    Update lambda-invoke.sh

--- lambda-invoke.sh
+++ lambda-invoke.sh
@@ -2,3 +2,11 @@ aws lambda invoke \
     --function-name {func-name} \
     --payload file://examples/basic-payload.json \
     response.json
+
+"""
+{"errorMessage": "Unable to import module 'lambda_function': No module named 'lambda_function'", "errorType": "Runtime.ImportModuleError", "requestId": "", "stackTrace": []}%             simrangulat@Simrans-MacBook-Pro-2 omnis-webData % aws lambda update-function-configuration \
+"""
+
+aws lambda update-function-configuration \
+    --function-name omnis-web-data-gen \
+    --handler lambda_handler.lambda_handler
